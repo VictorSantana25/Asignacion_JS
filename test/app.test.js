c35a2ee12962fee7263ce7abe93cd451cb32dbd6
V.Santana <drake.santana25@outlook.es>
2024-05-21 16:43:07 -0500

    deploy

--- test/app.test.js
+++ test/app.test.js
@@ -72,51 +72,52 @@ beforeEach(async () => {
 
 describe("Endpoints de Libros", () => {
 
-  test("Debería crear un nuevo libro", async () => {
-    const res = await request(app)
-      .post("/libros")
-      .send({ titulo: "Libro prueba", autor: "Autor prueba" });
+    test("Debería crear un nuevo libro", async () => {
+        const res = await request(app)
+            .post("/libros")
+            .send({ titulo: "Libro prueba", autor: "Autor prueba" });
     
-    expect(res.statusCode).toEqual(200);
-    expect(res.body.titulo).toEqual("Libro prueba");
-    expect(res.body.autor).toEqual("Autor prueba");
-  });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.titulo).toEqual("Libro prueba");
+        expect(res.body.autor).toEqual("Autor prueba");
+    });
 
-  test("Debería traer un listado de todos los libros", async () => {
-    await new Libro({ titulo: "Libro 1", autor: "Autor 1" }).save();
-    await new Libro({ titulo: "Libro 2", autor: "Autor 2" }).save();
+    test("Debería traer un listado de todos los libros", async () => {
+        await new Libro({ titulo: "Libro 1", autor: "Autor 1" }).save();
+        await new Libro({ titulo: "Libro 2", autor: "Autor 2" }).save();
 
-    const res = await request(app).get("/libros");
+        const res = await request(app).get("/libros");
 
-    expect(res.statusCode).toEqual(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body.length).toBe(2);
-  });
+        expect(res.statusCode).toEqual(200);
+        expect(Array.isArray(res.body)).toBe(true);
+        expect(res.body.length).toBe(2);
+    });
 
-  test("Debería obtener un libro específico por ID", async () => {
-    const libro = new Libro({ titulo: "Libro de prueba", autor: "Autor de prueba" });
-    await libro.save();
+    test("Debería obtener un libro específico por ID", async () => {
+        const libro = new Libro({ titulo: "Libro de prueba", autor: "Autor de prueba" });
+        await libro.save();
 
-    const res = await request(app).get(`/libros/${libro._id}`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('_id', libro._id.toString());
-    expect(res.body).toHaveProperty('titulo', libro.titulo);
-    expect(res.body).toHaveProperty('autor', libro.autor);
-  });
-
-
-  test("Debería eliminar un libro específico por ID", async () => {
-    const libro = new Libro({ titulo: "Libro de prueba", autor: "Autor de prueba" });
-    await libro.save();
-
-    const res = await request(app).delete(`/libros/${libro._id}`);
-    expect(res.statusCode).toEqual(204);
-
-    const libroInDb = await Libro.findById(libro._id);
-    expect(libroInDb).toBeNull();
-  });
+        const res = await request(app).get(`/libros/${libro._id}`);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('_id', libro._id.toString());
+        expect(res.body).toHaveProperty('titulo', libro.titulo);
+        expect(res.body).toHaveProperty('autor', libro.autor);
+    });
 
 
-afterAll(async () => {
-  await mongoose.connection.close();
+    test("Debería eliminar un libro específico por ID", async () => {
+        const libro = new Libro({ titulo: "Libro de prueba", autor: "Autor de prueba" });
+        await libro.save();
+
+        const res = await request(app).delete(`/libros/${libro._id}`);
+        expect(res.statusCode).toEqual(204);
+
+        const libroInDb = await Libro.findById(libro._id);
+        expect(libroInDb).toBeNull();
+    });
+
+
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
 });
